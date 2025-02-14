1a551addde36c57d3438fd709f19dc66db07dab2
00uno00 <edoardo.oglietti@edu.unito.it>
2025-02-14 17:44:13 +0100

    Fix configs name + update mass benchmark script

--- benchmarks.py
+++ benchmarks.py
@@ -51,33 +51,16 @@ alg_settings = [
      "step_size": 1,
      "model": "MNIST_2NN",
      "weighted": "true"
-     },    
-     {"batch_size": 10, 
-     "local_epochs": 10, 
-     "loss": "CrossEntropyLoss", 
-     "lr": 0.01,
-     "gamma": 1,
-     "step_size": 1,
-     "model": "ResNet18",
-     "weighted": "true"
-     },
-     {"batch_size": 32, 
-     "local_epochs": 10, 
-     "loss": "CrossEntropyLoss", 
-     "lr": 0.01,
-     "gamma": 1,
-     "step_size": 1,
-     "model": "ResNet18",
-     "weighted": "true"
      }
 ]
 
 exp_settings = [
-    {"config_name": "Simple",
+    {"config_name": "Simple_dir_0.3",
      "dataset_name": "mnist",
      "channel_dim": 1,
      "classes": 10,
-     "distribution_name": "iid", 
+     "distribution_name": "dir",
+     "beta": 0.3,
      "sampling_perc": 1, 
      "client_split": 0.2, 
      "keep_test": "true",  
@@ -91,12 +74,12 @@ exp_settings = [
      "eligible_perc": 0.2, 
      "n_clients": 50, 
      "n_rounds": 100},
-
-     {"config_name": "Complex",
-     "dataset_name": "cifar10",
-     "channel_dim": 3,
+     {"config_name": "Simple_dir_0.5",
+     "dataset_name": "mnist",
+     "channel_dim": 1,
      "classes": 10,
-     "distribution_name": "iid", 
+     "distribution_name": "dir",
+     "beta": 0.5,
      "sampling_perc": 1, 
      "client_split": 0.2, 
      "keep_test": "true",  
@@ -109,17 +92,25 @@ exp_settings = [
      "eval_every": 1,
      "eligible_perc": 0.2, 
      "n_clients": 50, 
-     "n_rounds": 200}
+     "n_rounds": 100}
 ]
 
 def main():
     for algorithm in algorithms:
         for exp_setting in exp_settings:
             for alg_setting in alg_settings :
+                print(f"Running experiment {exp_setting['config_name']} with {algorithm} and {alg_setting['model']}")
                 dataset = Datasets.get(exp_setting['dataset_name'], path="./data", channel_dim=int(exp_setting['channel_dim'] if alg_setting['model'] != "ResNet18" else None))
 
-                cfg = Configuration(f"./configs/exp_{algorithm}.yaml", f"./configs/fed{algorithm}.yaml")
-                dist_args=DDict(
+                #check if both config files exist
+                if not os.path.exists(f"./configs/exp_{algorithm}.yaml") :
+                    cfg = Configuration("./configs/exp_avg.yaml", f"./configs/fed{algorithm}.yaml")
+                    cfg.logger.tags = [f"{algorithm}"]
+                else :
+                    cfg = Configuration(f"./configs/exp_{algorithm}.yaml", f"./configs/fed{algorithm}.yaml")
+
+                
+                distr_args=DDict(
                     dataset=dataset,
                     distribution=exp_setting['distribution_name'],
                     sampling_perc=exp_setting['sampling_perc'],
@@ -127,11 +118,14 @@ def main():
                     keep_test=exp_setting['keep_test'],
                     server_test=exp_setting['server_test'],
                     server_split=exp_setting['server_split'],
-                    uniform_test=exp_setting['uniform_test']
-                    )
+                    uniform_test=exp_setting['uniform_test'],\
+                    #dist_args=DDict(
+                    #    beta=exp_setting['beta']
+                    #)
+                )
 
                 #data_splitter = DataSplitter(dataset=dataset, distribution=exp_setting['distribution_name'], dist_args=dist_args)
-                data_splitter = DataSplitter(**dist_args)
+                data_splitter = DataSplitter(**distr_args)
 
                 evaluator = ClassificationEval(exp_setting['eval_every'], n_classes=exp_setting['classes'])
 
@@ -141,6 +135,14 @@ def main():
                 settings.set_evaluator(evaluator=evaluator)
 
                 # Overwrite the hyperparameters
+                cfg.data.distribution.name=str(exp_setting['distribution_name'])
+                cfg.data.dataset.name=str(exp_setting['dataset_name'])
+                cfg.data.sampling_perc=int(exp_setting['sampling_perc'])
+                cfg.data.client_split=int(exp_setting['client_split'])
+                cfg.data.keep_test=bool(exp_setting['keep_test'])
+                cfg.data.server_test=bool(exp_setting['server_test'])
+                cfg.data.server_split=int(exp_setting['server_split'])
+                cfg.data.uniform_test=bool(exp_setting['uniform_test'])
                 cfg.method.hyperparameters.client.batch_size=int(alg_setting['batch_size'])
                 cfg.method.hyperparameters.client.local_epochs=int(alg_setting['local_epochs'])
                 cfg.method.hyperparameters.client.loss=str(alg_setting['loss'])
